874b5e24c85ef46610530a017e923e53d86cc997
Mr raju <ma8055965@gmail.com>
2024-06-05 10:04:17 +0500

    updated

--- src/Data/data.js
+++ src/Data/data.js
@@ -2393,23 +2393,86 @@ export const graphicFractionsWorksheets = [
     ],
   },
   {
-    title: "Draw the Fraction: Mixed Fractions 2",
+    title: "Fraction from Drawing: Simple Fractions",
     sheets: [
       {
-        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fractions-3-v1-medium.jpg",
-        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fractions-3-v1.html",
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-v1-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-v1.html",
       },
       {
-        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fractions-3-v2-medium.jpg",
-        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fractions-3-v2.html",
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-v2-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-v2.html",
       },
       {
-        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fractions-3-v3-medium.jpg",
-        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fractions-3-v3.html",
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-v3-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-v3.html",
       },
       {
-        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fractions-3-v4-medium.jpg",
-        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fractions-3-v4.html",
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-v4-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-v4.html",
+      },
+    ],
+  },
+  {
+    title: "Fraction from Drawing: Simple Fractions 2",
+    sheets: [
+      {
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-2-v1-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-2-v1.html",
+      },
+      {
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-2-v2-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-2-v2.html",
+      },
+      {
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-2-v3-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-2-v3.html",
+      },
+      {
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-2-v4-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/fraction-from-pie-graphic-2-v4.html",
+      },
+    ],
+  },
+  {
+    title: "Fraction from Drawing: Mixed Fractions 1",
+    sheets: [
+      {
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-3-v1-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-3-v1.html",
+      },
+      {
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-3-v2-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-3-v2.html",
+      },
+      {
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-3-v3-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-3-v3.html",
+      },
+      {
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-3-v4-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-3-v4.html",
+      },
+    ],
+  },
+  {
+    title: "Fraction from Drawing: Mixed Fractions 2",
+    sheets: [
+      {
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-4-v1-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-4-v1.html",
+      },
+      {
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-4-v2-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-4-v2.html",
+      },
+      {
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-4-v3-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-4-v3.html",
+      },
+      {
+        img: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-4-v4-medium.jpg",
+        url: "https://www.dadsworksheets.com/worksheets/graphic-fractions/mixed-fraction-from-pie-graphic-4-v4.html",
       },
     ],
   },
