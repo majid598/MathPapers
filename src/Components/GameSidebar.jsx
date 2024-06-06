742418cfac911b7f2f3af21c4868dac7302bf909
Mr raju <ma8055965@gmail.com>
2024-06-06 09:07:34 -0700

    Update GameSidebar.jsx

--- src/Components/GameSidebar.jsx
+++ src/Components/GameSidebar.jsx
@@ -6,38 +6,33 @@ const GameSidebar = ({ category, setCategory }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div
-      className={`lg:w-[22rem] w-[16rem] h-screen lg:pt-32 pt-24 fixed top-0 lg:left-0 z-50 ${
-        isOpen ? "left-0" : "-left-[16rem]"
-      } transition-all duration-500 lg:p-10`}
+      className={`lg:w-[22rem] w-[16rem] h-screen lg:pt-32 pt-24 fixed top-0 lg:left-0 z-50 ${isOpen ? "left-0" : "-left-[16rem]"
+        } transition-all duration-500 lg:p-10`}
     >
-      <div className="w-full h-full relative lg:border-2 border-r-2 overflow-hidden lg:rounded-tr-none lg:rounded-br-none rounded-tr-xl rounded-br-xl lg:bg-transparent bg-gray-900 lg:rounded-xl p-5">
+      <div className="w-full h-full relative lg:border-2 border-r-2 lg:overflow-hidden lg:rounded-tr-none lg:rounded-br-none rounded-tr-xl rounded-br-xl lg:bg-transparent bg-gray-900 lg:rounded-xl p-5">
         <button
           onClick={() => setIsOpen((prev) => !prev)}
           className="lg:hidden absolute -right-[3.45rem] border-t border-r border-b bg-gray-900 rounded-tr-full rounded-br-full h-12 py-1 flex flex-col justify-center px-3 pr-4 z-[999] top-4"
         >
           <span
-            className={`w-6 h-1 bg-white rounded-lg transition-all duration-500 ${
-              isOpen ? "rotate-45" : "my-0.5"
-            }`}
+            className={`w-6 h-1 bg-white rounded-lg transition-all duration-500 ${isOpen ? "rotate-45" : "my-0.5"
+              }`}
           ></span>
           <span
-            className={`w-6 h-1 bg-white my-0.5 rounded-lg transition-all duration-500 ${
-              isOpen ? "hidden" : "flex"
-            }`}
+            className={`w-6 h-1 bg-white my-0.5 rounded-lg transition-all duration-500 ${isOpen ? "hidden" : "flex"
+              }`}
           ></span>
           <span
-            className={`w-6 h-1 bg-white rounded-lg transition-all duration-500 ${
-              isOpen ? "-rotate-45" : "my-0.5"
-            }`}
+            className={`w-6 h-1 bg-white rounded-lg transition-all duration-500 ${isOpen ? "-rotate-45" : "my-0.5"
+              }`}
           ></span>
         </button>
         <h2 className="text-2xl font-semibold">Game Categories</h2>
         <div className="flex flex-col mt-5 overflow-y-scroll pr-4 pb-20 h-full">
           <button
             onClick={() => setCategory("")}
-            className={`w-full py-1 px-2 rounded-md text-start font-semibold ${
-              category === "" && "bg-white text-black"
-            }`}
+            className={`w-full py-1 px-2 rounded-md text-start font-semibold ${category === "" && "bg-white text-black"
+              }`}
           >
             All
           </button>
@@ -45,9 +40,8 @@ const GameSidebar = ({ category, setCategory }) => {
             <button
               key={i}
               onClick={() => setCategory(i)}
-              className={`w-full py-1 px-2 rounded-md text-start font-semibold ${
-                category === i && "bg-white text-black"
-              }`}
+              className={`w-full py-1 px-2 rounded-md text-start font-semibold ${category === i && "bg-white text-black"
+                }`}
             >
               {i}
             </button>
